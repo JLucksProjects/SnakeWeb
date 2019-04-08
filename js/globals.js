74a15370a6d6e5bab0011e96aa988568b4ba162a
JLucks <jluckssc@gmail.com>
2019-04-08 17:21:06 -0300

    #01 Inicio da função de movimentação

--- js/globals.js
+++ js/globals.js
@@ -1,17 +1,25 @@
-var PONTO = [];						//Array com os pontos
-var RETA = [];						//Array com as retas
-var AREA = [];						//Array com as areas
-var CIR = [];						//Array com os circulos
+var SNAKE = [];						//Array da snake
+var SPEED = 5;						//Speed da snake
+var FREQ = 1; 						//Frequência do alvo
+var NTARGET = 1;					//Número de alvos
+var TCURRENT = 0;					//Tipo do alvo
+var HIGHSCORE = 0;					//Pontuação
 var TOL = 5;						//Tolerância
-var MODEACTIVE = 1; 				//Modo de escrita, seleção ou exclusão
-var PRIMITIVE = 0;					//Modo ponto, reta, poligono ou circunferência
-var MODETRANSFORM = 0;				//Translação, Rotação, Espelhamento ou Escala, Area  e fecho
-var DRAWINGRETA = 0;				//Se esta desenhando uma reta
-var DRAWINGAREA = 0;				//Se esta desenhando uma area
-var DRAWINGCIR = 0;					//Se esta desenhando um circulo
-var DRAWINGTRANS = 0;				//Se esta transformando
-var SELECTID = -1;					//Primitiva selecionada
-var REF = 0;						//Referencia selecionada
-var ULTCOORD = {};					//Ultima coordenada
-var ULTCOORDs = [];					//Ultimas coordenadas
-var COORDSEL = {};					//Local selecionada
+var TTARGET = [1,5,10,50];			//Tipos de alvos
+/*Tipos
+	0 - Head up
+	1 - Head right
+	2 - Head left
+	3 - Head down
+	4 - Body hori
+	5 - Body vert
+	6 - Body up-right/left-down
+	7 - Body right-down/up-left
+	8 - Body right-up/down-left
+	9 - Body down-right/left-up
+	10 - Tail up
+	11 - Tail right
+	12 - Tail left
+	13 - Tail down
+	14 - Target
+*/				
